c1d8930ca2888aec466648529f4411d206f354d6
hootnot <f.brekeveld@gmail.com>
2016-02-25 12:49:03 +0100

    PEP8 compliant

--- postcodepy_proxy/views.py
+++ postcodepy_proxy/views.py
@@ -1,27 +1,41 @@
-"""
-Provides an PostcodepyProxyView class that 
-"""
+"""Postcodepy API ProxyView classes."""
 
-from django.views.generic  import View
+from django.views.generic import View
 
 from django.conf import settings
 from postcodepy import postcodepy
 
 USER_SETTINGS = getattr(settings, "POSTCODEPY", None)
 
+
 class PostcodepyProxyView(View):
-  def get(self, request, *args, **kwargs):
+    """PostcodeProxyView."""
 
-   api = postcodepy.API( environment='live', access_key=USER_SETTINGS['AUTH']['API_ACCESS_KEY'], access_secret=USER_SETTINGS['AUTH']['API_ACCESS_SECRET'])
-   pcat = ( kwargs['postcode'], kwargs['houseNumber'], kwargs['houseNumberAddition'] if kwargs.has_key('houseNumberAddition') else "" )
+    def get(self, request, *args, **kwargs):
+        """get."""
+        access_key = USER_SETTINGS['AUTH']['API_ACCESS_KEY']
+        access_secret = USER_SETTINGS['AUTH']['API_ACCESS_SECRET']
+        api = postcodepy.API(environment='live',
+                             access_key=access_key,
+                             access_secret=access_secret)
 
-   retValue = api.get_postcodedata( *pcat )
-   return retValue
+        pcat = (kwargs['postcode'], kwargs['houseNumber'])
+        if 'houseNumberAddition' in kwargs:
+            pcat = pcat + (kwargs['houseNumberAddition'],)
+
+        retValue = api.get_postcodedata(*pcat)
+        return retValue
+
 
 class SignalProxyView(View):
-  def get(self, request, sar, *args, **kwargs):
+    """SignalProxyView."""
 
-   api = postcodepy.API( environment='live', access_key=USER_SETTINGS['AUTH']['API_ACCESS_KEY'], access_secret=USER_SETTINGS['AUTH']['API_ACCESS_SECRET'])
-   retValue = api.get_signalcheck( sar )
-   return retValue
-
+    def get(self, request, sar, *args, **kwargs):
+        """get."""
+        access_key = USER_SETTINGS['AUTH']['API_ACCESS_KEY']
+        access_secret = USER_SETTINGS['AUTH']['API_ACCESS_SECRET']
+        api = postcodepy.API(environment='live',
+                             access_key=access_key,
+                             access_secret=access_secret)
+        retValue = api.get_signalcheck(sar)
+        return retValue
